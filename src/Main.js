4e9ddafb88998e8e456239d983e526004fcadec1
Maina Dhar <dmaina2244@gmail.com>
2022-12-31 20:31:44 -0800

    updated with more options

--- src/Main.js
+++ src/Main.js
@@ -11,6 +11,9 @@ import CityList from '../src/UI/CityList';
 import OptionList from '../src/UI/OptionList';
 import DetailList from '../src/UI/DetailList';
 import SurveyQuestions from '../src/UI/SurveyQuestions';
+import HealthBadge from '../src/UI/HealthBadge';
+import HealthBadgeColor from '../src/UI/HealthBadgeColor';
+import HealthBadgeColorPos from '../src/UI/HealthBadgeColorPos';
 
 const Main = () => {
     return (
@@ -24,6 +27,9 @@ const Main = () => {
                     <Stack.Screen name="Cities in Alameda County" component={CityList} />
                     <Stack.Screen name="City Details" component={DetailList} />
                     <Stack.Screen name="Survey Questions" component={SurveyQuestions} />
+                    <Stack.Screen name="Health Badge" component={HealthBadge} />
+                    <Stack.Screen name="Health Badge Negative" component={HealthBadgeColor} />
+                    <Stack.Screen name="Health Badge Positive" component={HealthBadgeColorPos} />
                 </Stack.Navigator>
             </NavigationContainer>
         </SafeAreaView>
